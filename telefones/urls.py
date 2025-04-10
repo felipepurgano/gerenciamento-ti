deff52665e51b4ed877552efa45000d681d04fc6
Felipe Purgano <felipepurgano@gmail.com>
2025-04-09 23:07:06 -0300

    Correções e adição de páginas

--- telefones/urls.py
+++ telefones/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('telefones/', views.telefones, name='telefones'),
+    path('cad_telefone/', views.cad_telefone, name='cad_telefone'),
 ]
--- telefones/views.py
+++ telefones/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def telefones(request):
     if request.method == 'GET':
         return render(request, 'telefones/telefones.html')
+    
+def cad_telefone(request):
+    if request.method == 'GET':
+        return render(request, 'telefones/cad_tel.html')

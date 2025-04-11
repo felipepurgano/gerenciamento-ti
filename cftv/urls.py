00112025ead425f6462e29645a011a5efc1b623c
Felipe Purgano <felipepurgano@gmail.com>
2025-04-11 00:21:16 -0300

    Collect Static, Criação das partials faltantes, Criação do botão Sair na sidebar

--- cftv/urls.py
+++ cftv/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('cftv/', views.cftv, name='cftv'),
+    path('cad_cftv/', views.cad_cftv, name='cad_cftv'),
 ]
--- cftv/views.py
+++ cftv/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def cftv(request):
     if request.method == 'GET':
         return render(request, 'cftv/cftv.html')
+    
+def cad_cftv(request):
+    if request.method == 'GET':
+        return render(request, 'cftv/cad_cftv.html')
